e59cfaee2c4880ed2e726b09bb700616ebed3346
eunji <lej7122@gmail.com>
2022-10-20 18:22:52 +0900

    Add item crud

--- src/main/java/portfolio/shop/web/admin/AdminController.java
+++ src/main/java/portfolio/shop/web/admin/AdminController.java
@@ -4,11 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import portfolio.shop.domain.item.Item;
 import portfolio.shop.domain.item.ItemSearchCond;
+import portfolio.shop.domain.item.ItemUpdateDto;
 import portfolio.shop.domain.member.Member;
 import portfolio.shop.service.item.ItemService;
 import portfolio.shop.service.member.MemberService;
@@ -34,6 +34,47 @@ public class AdminController {
         List<Item> items = itemService.findItems(itemSearch);
         model.addAttribute("items", items);
         return "admin/item/items";
+    }
+
+    @GetMapping("/items/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemService.findById(itemId).get();
+        model.addAttribute("item", item);
+        return "admin/item/item";
+    }
+
+    @GetMapping("/items/new")
+    public String addItem() {
+        return "admin/item/new";
+    }
+
+    @PostMapping("/items/new")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemService.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getDetail());
+        return "redirect:/admin/items";
+    }
+
+    @GetMapping("items/edit/{itemId}")
+    public String editItem(@PathVariable Long itemId, Model model) {
+        Item item = itemService.findById(itemId).get();
+        model.addAttribute("item", item);
+        return "admin/item/edit";
+    }
+
+    @PostMapping("items/edit/{itemId}")
+    public String editItem(@PathVariable Long itemId, @ModelAttribute ItemUpdateDto updateDto) {
+        itemService.update(itemId, updateDto);
+        return "redirect:/admin/items/{itemId}";
+    }
+
+    @PostMapping("/items/delete")
+    public String delete(@RequestParam(value = "checkbox-list", required = false) Long[] checkboxList) {
+        if (checkboxList != null) {
+            for (Long value : checkboxList)
+                itemService.delete(value);
+        }
+        return "redirect:/admin/items";
     }
 
     @GetMapping("/members")
